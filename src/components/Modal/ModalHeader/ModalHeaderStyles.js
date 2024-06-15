d3dfba599845311b1a0fe60397499bfd64d7aceb
Saviovd <savio.pinheiroal@gmail.com>
2024-06-14 22:45:04 -0400

    add: Modal Header style

---
+++ src/components/Modal/ModalHeader/ModalHeaderStyles.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export const ModalHeaderStyles = styled.div`
+margin-bottom: 2rem;
+   .project-name {
+      margin: 0;
+      font-size: 4rem;
+      color: rgba(var(--blue));
+   }
+   .project-description {
+      font-size: 1.5rem;
+      background-color: rgba(var(--grey-2), 0.3);
+      padding: 1rem;
+      border-radius: 0.6rem;
+   }
+   .project-main-language {
+      font-size: 1.5rem;
+
+      span {
+         font-size: 1.7rem;
+         color: rgba(var(--blue));
+         font-weight: 500;
+         margin-left: 1rem;
+      }
+   }
+
+   .dates {
+      display: flex;
+      gap: 3rem;
+   }
+   .project-created {
+      font-size: 1.5rem;
+      margin: 0;
+      margin: 3px 0;
+
+      span {
+         color: rgba(var(--grey));
+         background-color: rgba(var(--grey-2), 0.3);
+         padding: 0.5rem;
+         border-radius: 6px;
+         text-decoration: underline;
+      }
+   }
+`;
---
+++ src/components/Modal/ModalHeader/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { ModalHeaderStyles } from './ModalHeaderStyles';
+
+const formatWithLeadingZero = (number) => {
+   return number.toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
+   });
+};
+
+const formatDate = (dateString) => {
+   const date = new Date(dateString);
+   const day = formatWithLeadingZero(date.getDate());
+   const month = formatWithLeadingZero(date.getMonth() + 1);
+   const year = date.getFullYear();
+
+   return `${day}/${month}/${year}`;
+};
+const ModalHeader = ({ repoDetails }) => {
+   const createdDate = formatDate(repoDetails.created_at);
+   const updatedDate = formatDate(repoDetails.updated_at);
+   return (
+      <ModalHeaderStyles>
+         <h2 className='project-name'>
+            {repoDetails.name || 'No Name Available'}
+         </h2>
+         <p className='project-description'>
+            {repoDetails.description || 'No description available'}
+         </p>
+         <p className='project-main-language'>
+            Main language:{' '}
+            <span>{repoDetails.language || 'Not specified'}</span>
+         </p>
+         {repoDetails.license && (
+            <p className='project-main-language'>
+               License: {repoDetails.license.name}
+            </p>
+         )}
+         <div className='dates'>
+            <p className='project-created'>
+               Created at: <span>{createdDate}</span>
+            </p>
+            <p className='project-created'>
+               Last updated at: <span>{updatedDate}</span>
+            </p>
+         </div>
+      </ModalHeaderStyles>
+   );
+};
+
+export default ModalHeader;
