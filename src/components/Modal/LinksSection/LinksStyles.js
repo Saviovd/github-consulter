f46c414139a4c1d5605eb986621a16d94112e5b0
Saviovd <savio.pinheiroal@gmail.com>
2024-06-15 14:24:44 -0400

    add: Links responsiviness

--- src/components/Modal/LinksSection/LinksStyles.js
+++ src/components/Modal/LinksSection/LinksStyles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const LinksStyles = styled.div`
    margin-top: 2rem;
    display: flex;
+   align-items: center;
    gap: 2.5rem;
    font-weight: 300;
 
@@ -11,7 +12,8 @@ export const LinksStyles = styled.div`
       color: inherit;
       text-decoration: none;
       background-color: rgba(var(--grey));
-      padding: 0.5rem;
+      padding: 0rem 0.5rem;
+      height: 3rem;
       border-radius: 6px;
       display: flex;
       align-items: center;
@@ -19,5 +21,25 @@ export const LinksStyles = styled.div`
       &:hover {
          color: rgba(var(--dark-2));
       }
+
+      @media screen {
+         @media (max-width: 850px) {
+            font-size: 1.7rem;
+         }
+         @media (max-width: 850px) {
+            font-size: 1.5rem;
+         }
+         @media (max-width: 640px) {
+            font-size: 1.2rem;
+         }
+      }
    }
+   @media screen {
+         @media (max-width: 850px) {
+            gap: 1rem 1.5rem ;
+         }
+         @media (max-width: 640px) {
+            flex-wrap: wrap;
+         }
+      }
 `;
