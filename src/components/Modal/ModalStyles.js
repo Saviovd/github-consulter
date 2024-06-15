d56b6e77bc81d1aae4e19316304c8c48c73ecf5d
Saviovd <savio.pinheiroal@gmail.com>
2024-06-15 00:42:44 -0400

    add: modal styles

---
+++ src/components/Modal/ModalStyles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const ModalOverlay = styled.div`
+   position: fixed;
+   top: 0;
+   left: 0;
+   right: 0;
+   bottom: 0;
+   display: flex;
+   background: rgba(0, 0, 0, 0.7);
+   justify-content: center;
+   align-items: center;
+   z-index: 1000;
+   color: rgb(var(--light));
+`;
+
+export const ModalContent = styled.div`
+   background: rgba(var(--dark-2));
+   border: 1px solid rgba(var(--blue));
+   padding: 20px;
+   border-radius: 6px;
+   width: 80%;
+   height: 70%;
+   max-width: 80%;
+   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+   position: relative;
+   .close-button {
+      color: rgba(var(--blue));
+      text-decoration: underline;
+      background: none;
+      border: none;
+      font-size: 16px;
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      cursor: pointer;
+   }
+`;
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,10 +1,78 @@
-'use client'
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { closeModal } from '../../store/actions/modalActions';
-import styled from 'styled-components';
+import { ModalContent, ModalOverlay } from './ModalStyles';
+import ModalHeader from './ModalHeader';
+import ChartsSection from './ChartsSection';
+import ContributorsSection from './ContributorsSection';
+import LinksSection from './LinksSection';
+import axios from 'axios';
 
 const Modal = ({ isOpen, repoDetails, closeModal }) => {
+   const [contributors, setContributors] = useState([]);
+   const [languages, setLanguages] = useState([]);
+   const [stats, setStats] = useState({
+      commits: null,
+      issues: null,
+      pullRequests: null,
+   });
+
+   useEffect(() => {
+      const fetchContributors = async () => {
+         if (repoDetails && repoDetails.contributors_url) {
+            try {
+               const response = await fetch(repoDetails.contributors_url);
+               if (response.ok) {
+                  const data = await response.json();
+                  setContributors(data);
+               }
+            } catch (error) {
+               console.error('Error fetching contributors:', error);
+            }
+         }
+      };
+
+      const fetchLanguages = async () => {
+         if (repoDetails && repoDetails.languages_url) {
+            try {
+               const response = await fetch(repoDetails.languages_url);
+               if (response.ok) {
+                  const data = await response.json();
+                  setLanguages(data);
+               }
+            } catch (error) {
+               console.error('Error fetching languages:', error);
+            }
+         }
+      };
+
+      const fetchStats = async () => {
+         if (repoDetails) {
+            try {
+               const commitsResponse = await axios.get(`https://api.github.com/repos/${repoDetails.owner.login}/${repoDetails.name}/stats/commit_activity`);
+               const commitsData = commitsResponse.data;
+
+               const issuesResponse = await axios.get(`https://api.github.com/repos/${repoDetails.owner.login}/${repoDetails.name}/issues`);
+               const issuesData = issuesResponse.data;
+
+               const pullsResponse = await axios.get(`https://api.github.com/repos/${repoDetails.owner.login}/${repoDetails.name}/pulls`);
+               const pullsData = pullsResponse.data;
+
+               setStats({
+                  commits: commitsData.length,
+                  issues: issuesData.length,
+                  pullRequests: pullsData.length,
+               });
+            } catch (error) {
+               console.error('Error fetching repository statistics:', error);
+            }
+         }
+      };
+
+      fetchContributors();
+      fetchLanguages();
+      fetchStats();
+   }, [repoDetails]);
 
    if (!isOpen || !repoDetails) return null;
 
@@ -14,50 +82,24 @@ const Modal = ({ isOpen, repoDetails, closeModal }) => {
             <button className='close-button' onClick={closeModal}>
                Close
             </button>
-            <h2>{repoDetails.name}</h2>
-            <p>Repository Details:</p>
+            <ModalHeader repoDetails={repoDetails} />
+            <ChartsSection
+               repoDetails={repoDetails}
+               languages={languages}
+               commitsCount={stats.commits}
+               issuesCount={stats.issues}
+               pullsCount={stats.pullRequests}
+            />
+            <ContributorsSection contributors={contributors} />
+            <LinksSection repoDetails={repoDetails} />
          </ModalContent>
       </ModalOverlay>
    );
 };
 
-const mapStateToProps = (state) => {
-   return {
-      isOpen: state.modal.isOpen,
-      repoDetails: state.modal.repoDetails,
-   };
-};
+const mapStateToProps = (state) => ({
+   isOpen: state.modal.isOpen,
+   repoDetails: state.modal.repoDetails,
+});
 
 export default connect(mapStateToProps, { closeModal })(Modal);
-
-const ModalOverlay = styled.div`
-   position: fixed;
-   top: 0;
-   left: 0;
-   right: 0;
-   bottom: 0;
-   background: rgba(0, 0, 0, 0.7);
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   z-index: 1000;
-`;
-
-const ModalContent = styled.div`
-   background: white;
-   padding: 20px;
-   border-radius: 5px;
-   width: 500px;
-   max-width: 80%;
-   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-   position: relative;
-   .close-button {
-      background: none;
-      border: none;
-      font-size: 16px;
-      position: absolute;
-      top: 10px;
-      right: 10px;
-      cursor: pointer;
-   }
-`;
