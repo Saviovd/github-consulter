37a3b5aaacc1f1e5ddd0af8cb0e3f8e0a66c8a9a
Saviovd <savio.pinheiroal@gmail.com>
2024-06-16 00:22:28 -0400

    fix: header sizes

--- src/components/Search/SearchBarStyles.js
+++ src/components/Search/SearchBarStyles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const SearchBarStyles = styled.form`
-   background-color: rgba(var(--dark), .5);
+   background-color: rgba(var(--dark), 0.5);
    border: 1px solid rgba(var(--grey-2));
    padding: 0.2rem;
    display: flex;
@@ -9,15 +9,18 @@ export const SearchBarStyles = styled.form`
    justify-content: space-between;
    padding: 0 0.8rem;
    border-radius: 0.6rem;
-   height: 3rem;
-   width: 30rem;
+   height: 4rem;
+   width: 35rem;
    .octicon {
       fill: rgba(var(--grey));
       border-left: 1px solid rgba(var(--grey));
-      padding-left: 0.5rem;
+      padding-left: 0.8rem;
+      margin-right: 0.8rem;
+      height: 60%;
       cursor: pointer;
    }
    .input-text {
+      font-size: 1.7rem;
       background-color: transparent;
       border: 0;
       color: rgba(var(--grey));
@@ -27,11 +30,24 @@ export const SearchBarStyles = styled.form`
          outline: none;
          caret-color: var(--grey);
       }
+
+      @media (max-width: 420px) {
+         font-size: 1.5rem;
+      }
    }
 
    @media screen {
-      @media (max-width: 530px) {
+      @media (max-width: 610px) {
+         width: 30rem;
+      }
+      @media (max-width: 610px) {
+         width: 25rem;
+      }
+      @media (max-width: 420px) {
          width: 20rem;
+      }
+      @media (max-width: 370px) {
+         width: 18rem;
       }
    }
 `;
--- src/components/SortBox/SortBoxStyle.js
+++ src/components/SortBox/SortBoxStyle.js
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 export const SortBoxStyle = styled.label`
    border-radius: 0.6rem;
    border: 1px solid rgba(var(--grey-2));
-   background-color: rgba(var(--dark), .5);
+   background-color: rgba(var(--dark), 0.5);
    display: flex;
    align-items: center;
-   font-size: 1.4rem;
+   font-size: 1.8rem;
    padding: 0 0.2rem;
    color: rgba(var(--grey));
-   padding: .5rem;
+   padding: 0.8rem;
 
    .select {
       background-color: transparent;
@@ -17,11 +17,13 @@ export const SortBoxStyle = styled.label`
       color: rgba(var(--grey-2));
       width: 5rem;
       cursor: pointer;
+      font-size: 1.7rem;
 
       option {
+         font-size: 1.5rem;
          border-radius: 0.6rem;
          color: rgba(var(--light));
-         background-color: rgba(var(--grey-2, 0));
+         background-color: rgba(var(--grey-2, 0.1));
          border: 0;
          &:hover {
             background-color: rgba(var(--grey));
@@ -34,15 +36,15 @@ export const SortBoxStyle = styled.label`
          caret-color: var(--grey);
       }
       @media screen {
-      @media (max-width: 420px) {
-         width: 2rem;
+         @media (max-width: 480px) {
+            width: 2rem;
+         }
       }
-   }
    }
 
    @media screen {
-      @media (max-width: 420px) {
-         font-size: 1.2rem;
+      @media (max-width: 480px) {
+         font-size: 1.5rem;
       }
    }
 `;
